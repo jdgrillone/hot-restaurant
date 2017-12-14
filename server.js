c520316cd0b38773fd9a57a7d6915a169a3ea1da
Sorenious <30472130+Sorenious@users.noreply.github.com>
2017-12-13 20:44:44 -0700

    Added response and Package.json

--- server.js
+++ server.js
@@ -16,51 +16,9 @@ app.use(bodyParser.json());
 // Star Wars Characters (DATA)
 // =============================================================
 var reservations = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
 ];
 
 var waitList = [
-  {
-    routeName: "greedo",
-    name: "Greedo",
-    role: "Scumbag",
-    age: 250,
-    forcePoints: 0
-  },
-  {
-    routeName: "darthvader",
-    name: "Darth Vader",
-    role: "Dark Lord",
-    age: 55,
-    forcePoints: 2400
-  },
-  {
-    routeName: "darkprincexizor",
-    name: "Dark Prince Xizor",
-    role: "Crime Lord",
-    age: 55,
-    forcePoints: 0
-  }
 ];
 
 // Routes
@@ -95,21 +53,18 @@ app.post("/api/clear", function(req, res) {
 });
 
 // Create new reservations - takes in JSON input
-app.post("/api/new", function(req, res) {
+app.post("/api/tables", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   var newReservation = req.body;
 
   console.log(newReservation);
   if (reservations.length < 5) {
     reservations.push(newReservation);
-    return true;
+    res.json(true);
   } else {
     waitList.push(newReservation);
-    return false;
+    res.json(false);
   }
-  
-
-  res.json(newReservation);
 });
 
 // Starts the server to begin listening
